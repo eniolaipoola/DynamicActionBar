7de4f04f38d2381c40d0f5a7a842d3753f2eedc5
Eniola Ipoola <eniolaipoola@gmail.com>
2024-03-04 17:12:23 +0100

    Initial project setup

---
+++ app/src/main/java/com/example/dynamicactionbar/CustomToolBar.kt
@@ -0,0 +1,21 @@
+package com.example.dynamicactionbar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Class Description
+ * Created by Eniola Ipoola on 29/02/2024.
+ * Copyright (c) 2022 IDENTOS Inc. All rights reserved
+ */
+
+@Composable
+fun CustomToolBarComposable() {
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SearchViewPreview() {
+}
